f360a6cab72c6c181bf7290b33021a12228d1ef6
Hyesu Hong <honghs95@gmail.com>
2021-02-09 01:23:29 +0900

    print simple digital clock

--- clock.js
+++ clock.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('clock');
+// time boxes
+const time_period = document.querySelector('#PERIOD');
+const hours_box = document.querySelector('#HOURS');
+const minutes_box = document.querySelector('#MINUTES');
+const seconds_box = document.querySelector('#SECONDS');
+const weeks = document.querySelector('#WEEKS');
+
+function addZero(num) {
+  num < 10 ? (num = '0' + num) : (num = num);
+  return num;
+}
+
+setInterval(() => {
+  const today = new Date();
+  const day = today.getDay();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+
+  if (12 < hours) {
+    hours_box.innerText = addZero(hours - 12);
+    time_period.innerText = 'PM';
+  } else {
+    hours_box.innerText = addZero(hours);
+    time_period.innerText = 'AM';
+  }
+
+  minutes_box.innerText = addZero(minutes);
+  seconds_box.innerText = addZero(seconds);
+
+  weeks.children[day].classList.add('on');
+}, 1000);
